e51a20d5c0df44a09032030db0df4af801e867da
213njds <162525091+213njds@users.noreply.github.com>
2024-03-12 18:35:54 +0900

    2.c
    1에서 100까지 소수 모두 더하기

--- 2.c
+++ 2.c
@@ -1,7 +1,41 @@
-#include <stdio.h>
+/* #include <stdio.h>
+#define MAX_NUM 100
 
-int main(){
-    printf("안녕하세요.");
+int main()
+{
+    int num = 2, i;
+    
+    while(num <= MAX_NUM) {
+        i = 2;
 
+        while (i < num) {
+            if (num % i == 0) break;
+            i++;
+        }
+    if (num == 1) printf("%d", num);
+    num++;
+    }
+    printf("\n");
     return 0;
+}
+// 1~100까지 소수 출력하기 */
+
+#include<stdlib.h>
+#include<stdio.h>
+int main(void)
+{
+    int result = 0;
+    int k = 0;
+    for(int i=2; i<=100, i++)
+{
+    for (k=2; k<i;k++)
+{
+    if(i % k==0) break;
+}
+if(k == i)
+result += k;
+}
+printf("1부터 100까지 소수의 합 = %d\n, result");
+system("pause")
+
 }
